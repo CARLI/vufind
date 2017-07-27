482601860235009ff514c996feca442dca04a6be
Ed Schell <eschell@uillinois.edu>
2017-07-27 13:48:31 -0500

    PRC customization

--- themes/prc/config.php
+++ themes/prc/config.php
@@ -1,6 +1,6 @@
 <?
 
-$headerImage = 'vufind-prc.gif';
+$headerImage = 'vufind-prc.jpg';
 
 $headerUrl = 'http://library.principiacollege.edu';
 
@@ -9,13 +9,11 @@ $headerMap = '';
 $feedBackLink = 'mailto:libadmin@principia.edu';
 
 $introText = ' 
-<p>Welcome to the Principia Collegei\'s library catalog. Use the box above to search for library materials, request materials
+<p>Welcome to the Principia College\'s library catalog. Use the box above to search for library materials, request materials
 from I-Share, or check your library account.</p>
+';
 
-<p>Classic Search (the gold box in the upper right hand corner) links you
-to an interface that accommodates searching, selecting and compiling
-lists of resources with call numbers. Both contain the same
-information, just in different formats and functionality.</p>
+$loginText = '
 ';
 
 ?>
