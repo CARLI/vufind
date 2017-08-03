423977c550c3f071f70e3896d6d5672477720610
Ed Schell <eschell@uillinois.edu>
2017-08-03 10:34:08 -0500

    minor update to RMC feedback link, NIU customization css and image

--- themes/niu/config.php
+++ themes/niu/config.php
@@ -1,21 +1,18 @@
 <?
 
-$headerImage = 'vufind-niu.gif';
+$headerImage = 'vufind-niu.jpg';
 
-$headerUrl = '';
+$headerUrl = 'http://library.niu.edu/';
 
-$headerMap = ' <area shape="rect" coords="28,7,101,122" href="http://www.niu.edu/"
-alt="NIU">
-   <area shape="rect" coords="109,5,475,122" href="http://www.ulib.niu.edu/" alt="NIU Libraries">
-   <area shape="rect" coords="630,5,659,29" href="http://www.niu.edu/web.shtml" alt="A-Z">
-   <area shape="rect" coords="660,5,721,29" href="https://webcourses.niu.edu/webapps/portal/frameset.jsp" alt="Blackboard">
-   <area shape="rect" coords="722,5,774,29" href="https://calendar.niu.edu/" alt="Calendar">
-   <area shape="rect" coords="775,5,825,29" href="https://directory.niu.edu:8443/IDMProv" alt="Directory">
-   <area shape="rect" coords="826,5,867,29" href="http://www.niu.edu/myniu/" alt="MyNIU">
-   <area shape="rect" coords="868,5,928,29" href="http://www.niutoday.info" alt="NIU Today">
-   <area shape="rect" coords="788,30,928,73" href="http://www.niu.edu/niusearch.asp" alt="NIU Search">
+$headerMap = ' 
 ';
 
 $feedBackLink = 'http://www.ulib.niu.edu/';
+
+$introText ='
+';
+
+$loginText = '
+';
 
 ?>
