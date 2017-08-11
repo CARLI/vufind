b3fe13c0b8ae1d364120e0cd8c5fc9f2bdfebe87
Ed Schell <eschell@uillinois.edu>
2017-08-11 11:52:13 -0500

    very slight update to CARLI css, CSC and SXU customizations

--- themes/csc/config.php
+++ themes/csc/config.php
@@ -6,6 +6,15 @@ $headerUrl = 'http://www.sandburg.edu/library';
 
 $headerMap = '';
 
-$feedBackLink = 'mailto:library@sandburg.edu';
+$feedBackLink = 'http://www.sandburg.edu/Academics/Library/Ask-a-Librarian.html';
+
+$introText ='<p>Welcome to Carl Sandburg College\'s library catalog. </p>
+<p>To request or renew items you must set up an account through the Login link.
+Have your student ID number handy to complete the process.</p> 
+<p>To find journal or reference articles, search our other <a href="https://myportal.sandburg.edu/Library/Pages/Databases.aspx">databases</a>.</p>
+';
+
+$loginText = '
+';
 
 ?>
--- themes/sxu/config.php
+++ themes/sxu/config.php
@@ -8,6 +8,16 @@ $headerMap = '';
 
 $feedBackLink = 'mailto:library@sxu.edu';
 
-$introText = '';
+$introText = '<p>Welcome to the new SXU book and media search interface. Consider using the 
+"Advanced" search option for more precise searching (i.e. combinations of 
+authors and title keywords, keywords and subject headings, or LC call number 
+browsing). "Save to List" allows you to download specific records, and 
+"LOGIN" on the top right allows you to save favorites and to renew your 
+charged materials.</p>
+';
+
+$loginText = '<p>Barcode field should include the numbers below the barcode our your ID card 
+-- without the leading or trailing letters.</p>
+';
 
 ?>
