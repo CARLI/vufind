0009a9af1f2c21d6564a2a0cf82a75af59537a31
Ed Schell (CARLI) <eschell@uillinois.edu>
2017-12-21 10:05:10 -0600

    customizations for IIT CSC LNC

--- themes/csc/config.php
+++ themes/csc/config.php
@@ -1,6 +1,6 @@
-<?
+<?php
 
-$headerImage = 'vufind-csc.gif';
+$headerImage = 'vufind-csc.jpg';
 
 $headerUrl = 'http://www.sandburg.edu/library';
 
@@ -9,12 +9,18 @@ $headerMap = '';
 $feedBackLink = 'http://www.sandburg.edu/Academics/Library/Ask-a-Librarian.html';
 
 $introText ='<p>Welcome to Carl Sandburg College\'s library catalog. </p>
-<p>To request or renew items you must set up an account through the Login link.
-Have your student ID number handy to complete the process.</p> 
-<p>To find journal or reference articles, search our other <a href="https://myportal.sandburg.edu/Library/Pages/Databases.aspx">databases</a>.</p>
+<p>To request or renew items you must use the login link in the top right hand 
+corner. Have your student ID handy to complete the process. </p>
+<p>To find journal or reference articles, search our 
+<a href="https://myportal.sandburg.edu/Library/Pages/Databases.aspx">databases</a> page in 
+mySandbureg.</p> 
+
 ';
 
 $loginText = '
+<p>Your barcode is on the back of your Sandburg College ID card. You must use 
+"CSC" with your ID number. Example, CSC0123456. These fields are not case 
+sensitive.</p>
 ';
 
 ?>
