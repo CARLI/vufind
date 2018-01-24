1d0162dbeb8c3ee628b3708eb3e18f59de5bf67c
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-01-24 11:04:06 -0600

    customizations for NPU NIU NEI JUD

--- themes/jud/config.php
+++ themes/jud/config.php
@@ -1,11 +1,20 @@
 <?
 
-$headerImage = 'vufind-jud.gif';
+$headerImage = 'vufind-jud.png';
 
 $headerUrl = 'http://www.judsonu.edu/Library/';
 
 $headerMap = '';
 
 $feedBackLink = 'mailto:libraryhelp@judsonu.edu';
+
+$introText = '
+
+';
+
+$loginText = '
+<p>Your barcode is your library ID number. It is the 14-digit number on your
+Judson ID card.</p>
+';
 
 ?>
--- themes/nei/config.php
+++ themes/nei/config.php
@@ -1,11 +1,20 @@
 <?
 
-$headerImage = 'vufind-nei.gif';
+$headerImage = 'vufind-nei.jpg';
 
-$headerUrl = 'http://library.neiu.edu';
+$headerUrl = 'https://neiu.edu/library';
 
 $headerMap = '';
 
 $feedBackLink = 'http://libanswers.neiu.edu/search.php';
+
+$introText = '
+<p>Welcome to the new version of the NEIU Library / I-Share Catalog.</p>
+';
+
+$loginText = '
+<p>The barcode is the 14-digit number that appears on your NEIU ID card. It 
+begins with 21224... </p>
+';
 
 ?>
