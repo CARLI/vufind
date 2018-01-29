136cfb90917743a8bd796eb3006fbd87ece65ca0
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-01-29 14:15:15 -0600

    SIM customization and update to base CARLI css

--- themes/sim/config.php
+++ themes/sim/config.php
@@ -1,6 +1,6 @@
 <?
 
-$headerImage = 'vufind-sim.gif';
+$headerImage = 'vufind-sim.png';
 
 $headerUrl = 'http://www.siumed.edu/lib/';
 
@@ -8,14 +8,15 @@ $headerMap = '';
 
 $feedBackLink = 'http://www.siumed.edu/lib/forms/ask.html';
 
-$introtext = ' 
+$introText = ' 
 <p>Search for books, journal volumes and other materials held by the 
 SIUSM Library.</p>
 <p><strong>For more search options</strong>: Choose Advanced Search.</p>
 <p><strong>View your library account and renew books</strong>: Choose LOGIN</p>
 <p><strong>Request books from other libraries</strong>: Search All I-Share Libraries.</p>
-<p><strong>New to VuFind?: </strong><a href="https://vufind-test.carli.illinois.edu/vf-sim/MyResearch/Account?followup=%2Fvf-sim%2FMyResearch%2FHome">Create a new account</a>.</p>
-<p><strong>Need help or have a comment?</strong> Choose Ask Us or contact the Reference Desk (<a href="mailto:reference@siumed.edu">reference@siumed.edu</a> or 217-545-2113) or the Circulation Desk (217-545-2122).</p>
+<p><strong>Need help or have a comment?</strong> 
+Choose Ask Us or contact the Reference Desk (<a href="mailto:reference@siumed.edu">reference@siumed.edu</a> 
+or 217-545-2113) or the Circulation Desk (217-545-2122).</p>
 ';
 
 ?>
