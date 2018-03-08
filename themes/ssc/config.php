345d55218a402e331c5dc106ca114787aff2acae
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-03-08 12:27:14 -0600

    SSC customization, NBY banner link update

--- themes/ssc/config.php
+++ themes/ssc/config.php
@@ -10,4 +10,8 @@ $feedBackLink = 'mailto:ReferenceDesk@ssc.edu';
 
 $introText = '';
 
+$loginText = '
+<p>Library Barcode on back of SSC Student ID enter number with no spaces</p>
+<img src="https://www.ssc.edu/wp-content/themes/ssc/img/spheres.png" />
+'; 
 ?>
