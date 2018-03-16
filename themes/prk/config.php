d2af7986c2c9f2f31114ec29b56ad185d5c4fe77
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-03-16 10:11:53 -0500

    one minor correction to PRK home text link

--- themes/prk/config.php
+++ themes/prk/config.php
@@ -11,7 +11,7 @@ $feedBackLink = 'http://library.parkland.edu/contact';
 $introText = ' 
 <p>Find books, DVDs and other materials at the Parkland Library.
 Search over 80 other "I-Share" libraries and request materials.</p>
-<p>Need help? <a href="https://library.parkland.edu/contact">Contact us!</a></p>
+<p>Need help? <a href="http://library.parkland.edu/contact">Contact us!</a></p>
 ';
 
 $loginText = '
