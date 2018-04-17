aabe2155fe77fd94cfa360c366ebabf3e3e1f9c0
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-04-17 09:45:08 -0500

    customization for MLS and GSU, image only

--- themes/mls/config.php
+++ themes/mls/config.php
@@ -1,11 +1,22 @@
 <?
 
-$headerImage = 'vufind-mls.gif';
+$headerImage = 'vufind-mls.jpg';
 
 $headerUrl = 'http://www.meadville.edu/wiggin-library.php';
 
 $headerMap = '';
 
 $feedBackLink = 'mailto:libasst@meadville.edu';
+
+$introText = '
+<p>If you have any questions, contact Meadville Lombard Theological School\'s <a href="mailto:library@meadville.edu">Wiggin Library</a></p>
+';
+
+$loginText = '
+<p>If you have a library account, use the barcode number from the sticker on the back of your Meadville Lombard ID card. 
+If you do not know your barcode or do not have one, contact the <a href="mailto:library@meadville.edu">Wiggin Library</a>.</p>
+';
+
+$searchDropText = 'Meadville Lombard Catalog Only';
 
 ?>
