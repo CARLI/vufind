b8278f37d0caf8e552a648a669671ce85edf6ffc
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-04-24 13:49:14 -0500

    customization for MIL & UIU, update to carli.css and advanced search layout

--- themes/mil/config.php
+++ themes/mil/config.php
@@ -8,4 +8,13 @@ $headerMap = '';
 
 $feedBackLink = 'http://www.millikin.edu/staley';
 
+$loginButtonText = 'LOGIN WITH <span class="text-transform-none">myMILLIKIN</span>'; 
+
+$loginLinkHelp = 'Don\'t have a myMILLIKIN login?'; 
+
+$loginText = ' 
+<p>If you do not have a myMILLIKIN Username and Password, please enter your 
+Library Borrower ID/Barcode and your Last Name, then click Login.</p> 
+'; 
+
 ?>
