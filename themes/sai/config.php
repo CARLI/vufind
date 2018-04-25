a6a4eb169a795aeb6ddd52d9c76334498aae5583
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-04-25 14:53:54 -0500

    customization for SAI

--- themes/sai/config.php
+++ themes/sai/config.php
@@ -1,23 +1,19 @@
 <?
 
-$headerImage = 'vufind-sai.gif';
+$headerImage = 'vufind-sai.jpg';
 
 $headerUrl = '';
 
-$headerMap = '<area shape="rect" coords="297,-2,643,45" href="http://vufind-test.carli.illinois.edu/vf-sai/" title="Flaxman Library Catalog" alt="Flaxman Library Catalog" />
-<area shape="rect" coords="648,0,951,46" href="http://www.saic.edu/library" title="John M. Flaxman Library" alt="John M. Flaxman Library" />
-<area shape="rect" coords="648,0,957,46" href="javascript:;" alt="" />
-<area shape="rect" coords="-12,37,227,117" href="http://www.saic.edu/" title="School of the Art Institute of Chicago" alt="School of the Art Institute of Chicago" />
-<area shape="poly" coords="917,88,945,88,945,116,917,116,917,88" href="http://www.delicious.com/saic_library" title="Flaxman Library\'s Bookmarks on delicious" alt="Flaxman Library\'s Bookmarks on delicious" />
-<area shape="poly" coords="889,88,917,88,917,116,889,116,889,88" href="http://saiclibrary.tumblr.com/" title="Flaxman Library on Tumblr" alt="Flaxman Library on Tumblr" />
-<area shape="poly" coords="861,88,889,88,889,116,861,116,861,88" href="https://www.facebook.com/saic.library" title="Flaxman Library on Facebook" alt="Flaxman Library on Facebook" />
-<area shape="poly" coords="833,88,861,88,861,116,833,116,833,88" href="https://twitter.com/saic_library" title="@saic_library on Twitter" alt="@saic_library on Twitter" />
-<area shape="rect" coords="829,71,945,87" href="mailto:flaxman@saic.edu" alt="" />
+$headerMap = '
+<area shape="rect" coords="176,0,491,175" href="http://www.saic.edu/academics/librariesandspecialcollections/johnmflaxmanlibrary/" title="John M. Flaxman Library" alt="John M. Flaxman Library" />
+<area shape="rect" coords="0,0,165,175" href="http://saic.edu/" title="School of the Art Institute of Chicago" alt="School of the Art Institute of Chicago" />
 ';
 
 $feedBackLink = 'mailto:flaxman@saic.edu';
 
 $introText = ' 
 ';
+
+$searchDropText = 'SAIC Flaxman Library';
 
 ?>
