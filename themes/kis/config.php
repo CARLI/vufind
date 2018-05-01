06c444eeac8fcedb187ca33ee6aa7f0022e754bc
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-05-01 10:07:04 -0500

    KIS customization

--- themes/kis/config.php
+++ themes/kis/config.php
@@ -2,10 +2,27 @@
 
 $headerImage = 'vufind-kis.gif';
 
-$headerUrl = 'http://www.kishwaukeecollege.edu/student_services/library/index.php?page=89';
+$headerUrl = '/vf-kis/';
 
 $headerMap = '';
 
-$feedBackLink = 'http://www.kishwaukeecollege.edu/student_services/library/suggestions.php?page=89';
+$feedBackLink = 'https://www.kish.edu/services-library/library-general-information';
+
+$introText = '
+<p><strong>Your Library Account:</strong> Renew your checkouts and order books from other libraries using the I-Share system. 
+<a href="/vf-kis/MyResearch/UserLogin">Set up your free I-Share account</a>.
+</p>
+
+<p>&nbsp;</p>
+
+<p>Kishwaukee College • 21193 Malta Road • Malta, Illinois 60150 • 815-825-2086 • TTY 815-825-9106</p>
+';
+
+$loginText = '
+<p>Use your 7 digit Kish ID number as your "barcode" to create an I-Share
+account.</p>
+';
+
+$searchDropText = 'Kishwaukee Only';
 
 ?>
