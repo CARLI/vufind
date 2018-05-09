faec5395afe4b19249a5ba15ced19c955c04185d
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-05-09 14:57:00 -0500

    BEN and SIC customization

--- themes/ben/config.php
+++ themes/ben/config.php
@@ -1,17 +1,23 @@
 <?
 
-$headerImage = 'vufind-ben.gif';
+$headerImage = 'vufind-ben.png';
 
-$headerMap = '<area shape="rect" coords="13,16,226,59" href="http://ben.edu/" alt="Benedictine University" />
-<area shape="rect" coords="163,113,224,160" href="http://ben.edu/library/" alt="Home" />
-<area shape="rect" coords="231,113,326,160" href="http://researchguides.ben.edu/az.php" alt="Databases" />
-<area shape="rect" coords="332,114,470,160" href="http://ben.edu/library/ILL/index.cfm" alt="Interlibrary Loan" />
-<area shape="rect" coords="474,114,575,160" href="http://ben.edu/library/faculty/index.cfm" alt="For Faculty" />
-<area shape="rect" coords="577,114,635,159" href="http://benulibrary.ask.libraryh3lp.com/" alt="FAQ" />
-<area shape="rect" coords="636,114,724,158" href="http://ben.edu/library/archives/index.cfm" alt="Archives" />
-<area shape="rect" coords="726,115,791,159" href="http://ben.edu/library/about.cfm" alt="About" />
-<area shape="rect" coords="825,67,936,104" href="http://ben.edu/library/contact.cfm" alt="Chat now" />
-<area shape="rect" coords="7,65,123,108" href="http://ben.edu/library/" alt="Benedictine University Library" />';
+$headerUrl = 'http://www.ben.edu/library/index.cfm';
 
 $feedBackLink = 'http://www.ben.edu/library/contact.cfm';
+
+$introText = '
+<p>Welcome to the Benedictine University Library catalog.
+Login to request or renew materials.
+Return to the <a href="http://www.ben.edu/library/index.cfm"> Benedictine
+Library homepage. </a></p>
+'; 
+
+$searchDropText = 'Benedictine University'; 
+
+$loginText = '
+<p>Your barcode is 2281100 plus your b-number without the "b" (example:
+22811001234567). This barcode number is on the back of your university ID.</p>
+';
+
 ?>
