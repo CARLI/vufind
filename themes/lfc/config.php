0614e1d0a8d57d465d6d7d59d1542ae05a73173d
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-07-11 10:11:50 -0500

    EIU & LFC customization

--- themes/lfc/config.php
+++ themes/lfc/config.php
@@ -28,4 +28,11 @@ borrower account, please enter your borrower number and your last name. To
 verify your borrower number, please email <a href="mailto:libcirc@lakeforest.edu">libcirc@lakeforest.edu</a>.</p>
 '; 
 
+$catEstablishAccount = '
+<p>Your barcode is located on the front of your College ID below your photo.
+Enter all 14 numbers beginning with the 2. If you need assistance logging in,
+please email libcirc@lakeforest.edu or call the Donnelley and Lee Library
+circulation desk at 847-735-5056.</p>
+'; 
+
 ?>
