7ecaba851ce7787102783215cca40e0e5cfacb99
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-08-01 12:47:51 -0500

    UIU, NIU, MLS, MBI, ISU customizations

--- themes/niu/config.php
+++ themes/niu/config.php
@@ -1,6 +1,6 @@
 <?
 
-$headerImage = 'vufind-niu.jpg';
+$headerImage = 'vufind-niu.png';
 
 $headerUrl = 'http://library.niu.edu/';
 
@@ -11,21 +11,23 @@ $feedBackLink = 'http://www.ulib.niu.edu/';
 
 $introText ='
 <p>&nbsp;</p>
-<p>If you are using this version of VuFind for the <strong>first time</strong>, you are required to set 
-up your account profile by entering library, barcode and last name. This information can be retrieved 
-from your valid NIU OneCard. If your profile was not linked to your library account successfully, 
-you will be prompted to finish the set-up of your library catalog profile every time you click on one 
-of the categories listed in "Your Account". You will be able to request items from the catalog as 
-soon as this step has been completed successfully.</p>
-<p>Please note saved items (lists and favorites) will <strong>not</strong> be migrated from 
-previous versions of VuFind.</p>
+<p>If you are using I-Share VuFind for the <strong>first time</strong>, 
+you are required to set up your account profile by entering library, barcode and 
+last name. This information can be retrieved from your valid NIU OneCard. If your 
+profile was not linked to your library account successfully, you will be prompted to 
+finish the set-up of your library catalog profile every time you click on one of the 
+categories listed in "Your Account". You will be able to request items from the catalog 
+as soon as this step has been completed successfully.</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 ';
 
 $loginText = '
-<p>If you do not have a NIU AccountID, please enter the last name and barcode
-of your NIU Onecard to proceed.</p>
+<p>&nbsp;</p>
+<p>If you are using I-Share VuFind for the <strong>first time</strong>, you are required to set up your account profile by entering library, barcode and last name. This information can be retrieved from your valid NIU OneCard. If your profile was not linked to your library account successfully, you will be prompted to finish the set-up of your library catalog profile every time you click on one of the categories listed in "Your Account". You will be able to request items from the catalog as soon as this step has been completed successfully.</p>
+<p>Please note saved items (lists and favorites) will <strong>not</strong> be migrated from previous versions of VuFind.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 ';
 
 $loginLinkHelp = 'Don\'t have a NIU AccountID?'; 
