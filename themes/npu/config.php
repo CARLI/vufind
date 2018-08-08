87817857d25e968c4e89bd4f9c969f3c7495b859
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-08-08 14:04:12 -0500

    NPU single sign customization

--- themes/npu/config.php
+++ themes/npu/config.php
@@ -10,4 +10,10 @@ $feedBackLink = 'mailto:reference@northpark.edu';
 
 $introText ='';
 
+$catEstablishAccount = '
+<p>Your barcode is on the back of your North Park University ID card starting
+with “2701200”. If you have any trouble logging into your account, please
+contact Brandel Library at (773) 244-5580 or library@northpark.edu.</p>
+'; 
+
 ?>
