3003d3cda25f5a6b6f37043b0b6c89863cc67b45
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-09-07 12:27:42 -0500

    NCC singlesignon customization

--- themes/ncc/config.php
+++ themes/ncc/config.php
@@ -4,10 +4,28 @@ $headerImage = 'vufind-ncc.gif';
 
 $headerUrl = '';
 
-$headerMap = '<area shape="rect" coords="115,79,-1,-1" href="http://www.noctrl.edu" alt="North Central College">
+$headerMap = '
+<area shape="rect" coords="115,79,-1,-1" href="http://www.noctrl.edu" alt="North Central College">
 <area shape="rect" coords="630,80,120,-1" href="http://library.noctrl.edu" alt="Oesterle Library">
 ';
 
 $feedBackLink = 'http://library.noctrl.edu/help/';
+
+$loginButtonText = 'Student, faculty & staff LOGIN';
+
+$loginLinkHelp = 'Alumni & Courtesy Borrower Login';
+
+$loginText = '
+<h4><strong>Alumni & Courtesy Borrower</strong></h4>
+<p>If you are an alumni or courtesy borrower, enter your library Barcode number
+and last name, then click login.</p>
+';
+
+$catEstablishAccount = '
+<p>This is a one-time account setup. In order to establish your account
+profile, please enter the following information. Note that your Barcode
+number can be found on the back of your NCC ID. If you have difficulty
+logging in, contact library staff at 630-637-5715</p>
+';
 
 ?>
