353f5e7e156f89bec31e382f5cbd0e92349ea933
Chris Delis (CARLI) <cedelis@uillinois.edu>
2018-10-01 13:32:40 -0500

    GitHub Issue #320 Stackmap for UIC

--- themes/uic/theme.config.php
+++ themes/uic/theme.config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
-    'extends' => 'carli'
+    'extends' => 'carli',
+    'js' => array(
+        'https://www.stackmap.com/integration/uic-vufindv4/StackMap.js',
+    ),
+    'css' => array(
+        'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
+        'https://www.stackmap.com/integration/uic-vufindv4/StackMap.css',
+    ),
 );
