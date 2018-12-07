53293848065598588a2589493a4562628d4d72c5
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-12-07 08:18:26 -0600

    IVC customization

--- themes/ivc/config.php
+++ themes/ivc/config.php
@@ -8,4 +8,20 @@ $headerMap = '';
 
 $feedBackLink = 'mailto:Jacobs_Library@ivcc.edu';
 
+$introText = '
+<p>Need help? Contact Jacobs Library at 815-224-0306 or
+<a href="mailto:Jacobs_Library@ivcc.edu">Jacobs_Library@ivcc.edu</a>.</p>
+<p>Please note the library catalog sends email correspondence to your IVCC email
+account.</p>
+';
+
+$loginText = '
+<p>Your 14-digit barcode is found on the bottom of your IVCC ID card:
+24611XXXXXXX01. To build the number, type 24611, then your 7-digit number,
+then 01 (or 02, 03, etc. to match any replacement ID cards.) To find your
+7-digit number, check in WebAdvisor.</p>
+<p>If you are unable to log in, please
+contact Jacobs Library at 815-224-0306.</p>
+';
+
 ?>
