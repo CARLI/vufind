972b1ec446aa4d9cb616f4f7c7f7551ae5ad7f63
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-12-11 13:09:30 -0600

    KNX customization, UIS correction

--- themes/uis/config.php
+++ themes/uis/config.php
@@ -9,10 +9,6 @@ $headerMap = '';
 $feedBackLink = 'https://illinois.edu/fb/sec/8979413';
 
 $introText = ' 
-<p>Users must <a href
-="https://vufind-test.carli.illinois.edu/all/vf/MyResearch/Account?followup=%2Fa
-ll%2Fvf%2FMyResearch%2FHome"> create a new account </a> the first time
-they request items through the catalog.</p>
 ';
 
 $loginButtonText = 'Login with your NetID'; 
