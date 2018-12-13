a6c0738c23bff5f0b2170ad5aa417e145f786ef3
Chris Delis (CARLI) <cedelis@uillinois.edu>
2018-12-13 09:42:05 -0600

    GitHub Issue #320
    DPU has modified/fixed their stackmap javascript.

--- themes/dpu/theme.config.php
+++ themes/dpu/theme.config.php
@@ -2,12 +2,10 @@
 return array(
     'extends' => 'carli',
     'js' => array(
-        //'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
-        //'https://www.stackmap.com/integration/depaul_vufindv4/StackMap.js',
-        'vendor/dpu_stackmap.js',
+        'https://stackmap.com/integration/depaul_vufindv4/StackMap.js',
     ),
     'css' => array(
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
-        'https://www.stackmap.com/integration/depaul_vufindv4/StackMap.css',
+        'https://stackmap.com/integration/depaul_vufindv4/StackMap.css',
     ),
 );
