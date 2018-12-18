35266f7c0e9a5fe6aafcad9accf94076fd0176a3
Ed Schell (CARLI) <eschell@uillinois.edu>
2018-12-18 14:18:54 -0600

    Undo SSO for KNX

--- themes/knx/config.php
+++ themes/knx/config.php
@@ -22,22 +22,8 @@ libcirc@knox.edu">libcirc@knox.edu</a>.</p>
 $searchDropText = 'Knox College Catalog'; 
 
 $loginText = '
-<p>Login using your barcode and last name. Your barcode is found on the back of
-your Knox ID card. Enter <strong>only the numbers</strong> of the barcode--do
-not use the letters. If you do not have a barcode, please contact a member of
-the Knox Library staff or email <a href="mailto:reference@knox.edu">reference@knox.edu.</a></p>
+<p>Your barcode is found on the back of your Knox ID card. Enter <strong>only
+the numbers</strong> of the barcode--do not use the letters.</p>
 '; 
-
-$loginButtonText = 'Login with your Knox credentials.'; 
-
-$loginLinkHelp = 'Don\'t have Knox credentials?'; 
-
-$catEstablishAccount = '
-<p>The <strong>first</strong> time you login into your library account using
-your Knox credentials, you are required to set up your account profile by
-entering library, barcode and last name. Note: Your barcode is found on the
-back of your Knox ID card. Enter <strong>only the numbers</strong> of the
-barcode--do not use the letters. </p>
-';
 
 ?>
